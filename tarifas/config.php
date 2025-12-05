f5883712433ff448df71b2634aa7a19ce686416a
Ernesto Pineda <camoril@gmail.com>
2025-12-05 11:34:42 -0600

    Sanitize config.php: Remove database credentials

--- tarifas/config.php
+++ tarifas/config.php
@@ -2,7 +2,7 @@
 // Configuración de Base de Datos
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'zadmin_tarifas');
-define('DB_USER', 'tarifaswtc');
-define('DB_PASS', '4utedasyq');
+define('DB_USER', 'usuario_db');
+define('DB_PASS', 'password_db');
 define('DB_CHARSET', 'utf8mb4');
 ?>
